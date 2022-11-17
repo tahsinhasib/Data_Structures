e882af6073f11a10941946829a749f10cdd557d6
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2022-11-17 11:59:41 +0600

    Add files via upload

---
+++ Binary Search Tree/Binary_Search_Tree.cpp
@@ -0,0 +1,114 @@
+#include<iostream>
+using namespace std;
+
+struct BstNode{
+	int data;
+	BstNode *left;
+	BstNode *right;
+};
+
+BstNode *root;
+
+BstNode *GetNewNode(int data)
+{
+	BstNode *NewNode = new BstNode();
+	NewNode->data = data;
+	NewNode->left = NULL;
+	NewNode->right = NULL;
+	return NewNode;
+}
+
+void PreOrder(BstNode *root){
+	if(root==NULL){
+		return;
+	}
+	cout << root->data << " ";
+	
+    PreOrder(root->left); //Recursion
+	PreOrder(root->right); //Recursion
+}
+
+void InOrder(BstNode *root){
+
+	InOrder(root->left);
+    
+    if(root==NULL){
+		return;
+	}
+	cout << root->data << " ";
+
+	InOrder(root->right);
+}
+
+void PostOrder(BstNode *root){
+	
+	PostOrder(root->left);
+    PostOrder(root->right);
+
+    if(root==NULL){
+		return;
+	}
+	cout << root->data << " ";
+}
+
+
+
+BstNode *Insert(BstNode *root, int data){
+	if(root==NULL){
+		root = GetNewNode(data);
+	}
+	else if (data <= root->data){
+		root->left = Insert(root->left, data);
+	}
+	else{
+		root->right = Insert(root->right, data);
+	}
+	return root;
+}
+
+bool Search(BstNode *root, int data){
+	if(root==NULL){
+		cout << "Error: tree is empty" << endl;
+		return false;
+	}
+	else if(root->data == data){
+		return true;
+	}
+	else if(data <= root->data){
+		return Search(root->left, data);
+	}
+	else{
+		return Search(root->right, data);
+	}
+}
+
+int main()
+{
+	root = NULL;
+    
+	root = Insert(root, 5);
+	root = Insert(root, 6);
+	root = Insert(root, 4);
+	root = Insert(root, 7);
+	root = Insert(root, 3);
+	root = Insert(root, 2);
+
+	cout << "please enter your search item: ";
+
+	int s;
+	cin >> s;
+	cout << "\n";
+
+	if(Search(root, s) == true){
+		cout << "found" << endl;
+	}
+	else{
+		cout << "Not found" << endl;
+	}
+
+	PreOrder(root);
+
+}
+
+
+
