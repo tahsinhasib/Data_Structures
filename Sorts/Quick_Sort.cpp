aeee639a3e668c3448055ac65fd6018c66b9643f
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2023-01-07 23:43:36 +0600

    Update Quick_Sort.cpp

--- Sorts/Quick_Sort.cpp
+++ Sorts/Quick_Sort.cpp
@@ -1,64 +1,57 @@
-/*
-    C++ Implementation of the Quick Sort Algorithm.
-*/
-
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
+void quickSort(int arr[], int left, int right) {
+    int i = left, j = right;
+    int tmp;
+    int pivot = arr[(left + right) / 2];
 
-int partition(int arr[], int start, int end){
-
-	int pivot = arr[start];
-	int count = 0;
-    
-	for(int i=start+1; i<=end; i++){
-		if(arr[i] <= pivot){
-            count++;
+    /* partition */
+    while (i <= j) {
+        while (arr[i] < pivot){
+            i++;
         }
-	}
-	int pivotIndex = start + count;                 // Giving pivot element its correct position
-	swap(arr[pivotIndex], arr[start]);
-	int i=start, j=end;                             // Sorting left and right parts of the pivot element
+        while (arr[j] > pivot){
+            j--;
+        }
+        if (i <= j) {
+            tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+            i++;
+            j--;
+        }
+    }
 
-	while(i<pivotIndex && j>pivotIndex){
-
-		while(arr[i] <= pivot){
-			i++;
-		}
-		while(arr[j] > pivot) {
-			j--;
-		}
-		if(i<pivotIndex && j>pivotIndex) {
-			swap(arr[i++], arr[j--]);
-		}
-	}
-	return pivotIndex;
+    /* recursion */
+    if (left < j){
+        quickSort(arr, left, j);
+    }
+    if (i < right){
+        quickSort(arr, i, right);
+    }
 }
 
+int main() {
 
-void quickSort(int arr[], int start, int end){
+    cout << "\n";
+    int arr[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int n = sizeof(arr)/sizeof(arr[0]);
 
-	if(start >= end){
-        	return;                                     // base case
-    	}                               
-	int p = partition(arr, start, end);             // partitioning the array
-	quickSort(arr, start, p-1);                     // Sorting the left part
-	quickSort(arr, p+1, end);                       // Sorting the right part
+    cout << "The unsorted array: ";
+    for (int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+
+    cout << "\n";
+    quickSort(arr, 0, n-1);
+
+    cout << "The sorted array: ";
+    for (int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+
+    cout << "\n";
+    return 0;
 }
-
-
-int main()
-{
-	int arr[] = {9, 3, 4, 2, 1, 8};
-	int n = 6;
-
-	quickSort(arr, 0, n-1);
-
-	for(int i=0; i<n; i++){                         // printing the sorted array
-		cout << arr[i] << " ";
-	}
-
-	return 0;
-}
-
-
